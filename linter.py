cb05bab233a10d4725962f45b9285b3ff5638ba2
Ethan Zimmerman <thebinarypenguin@gmail.com>
2014-12-26 21:39:19 -0500

    Initial Template Created

---
+++ linter.py
@@ -0,0 +1,36 @@
+#
+# linter.py
+# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
+#
+# Written by Ethan Zimmerman,,,
+# Copyright (c) 2014 Ethan Zimmerman,,,
+#
+# License: MIT
+#
+
+"""This module exports the RamlCop plugin class."""
+
+from SublimeLinter.lint import NodeLinter, util
+
+
+class RamlCop(NodeLinter):
+
+    """Provides an interface to raml-cop."""
+
+    syntax = ''
+    cmd = 'raml-cop'
+    executable = None
+    version_args = '--version'
+    version_re = r'(?P<version>\d+\.\d+\.\d+)'
+    version_requirement = '>= 1.0'
+    regex = r''
+    multiline = False
+    line_col_base = (1, 1)
+    tempfile_suffix = None
+    error_stream = util.STREAM_BOTH
+    selectors = {}
+    word_re = None
+    defaults = {}
+    inline_settings = None
+    inline_overrides = None
+    comment_re = r'\s*/[/*]'
